c60b11fee15097503850feb78791a3a5f8e0d0b5
James <jsld1975@gmail.com>
2019-05-13 12:23:31 -0400

    local fix

--- src/updatetomax.bat
+++ src/updatetomax.bat
@@ -1,2 +1,3 @@
 copy ".\x64\max2020_AVX2\BerconMaps_2020_x64_AVX2.dlt" "C:\Program Files\Autodesk\3ds Max 2020\Plugins"
+copy ".\x64\max2019_AVX2\BerconMaps_2019_x64_AVX2.dlt" "C:\Program Files\Autodesk\3ds Max 2019\Plugins"
 pause
